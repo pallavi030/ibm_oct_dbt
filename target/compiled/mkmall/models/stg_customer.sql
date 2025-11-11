5609317244010aa15d8921a9eafb994aa8a92a6a
Pallavi030 <pallavirkg2004@gmail.com>
2025-11-11 17:02:57 +0530

    commit_02

---
+++ target/compiled/mkmall/models/stg_customer.sql
@@ -0,0 +1,9 @@
+select c_custkey as customers_kid,
+       c_name as name,
+       c_address as address,
+       c_nationkey as nation_id,
+       c_phone as phone_number,
+       c_acctbal as account_balance,
+       c_mktsegment as market_segment,
+       c_comment as comment
+ from sourcedb.mk_mall.customers
